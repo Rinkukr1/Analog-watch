0d3701333229645daffbda1ceccc8e79d8fb376a
Rinkukr1 <rinku5060saini@gmail.com>
2024-01-20 16:36:31 +0530

    Clock uploaded

---
+++ script.js
@@ -0,0 +1,22 @@
+let hours=document.getElementById('hours');
+let min=document.getElementById('min');
+let sec=document.getElementById("sec");
+
+function showDate(){
+    let date=new Date();
+
+    let hh=date.getHours();
+    let mm=date.getMinutes();
+    let scc=date.getSeconds();
+
+    let hourRotate=hh*30+mm/2;
+    let minRotate=mm*6;
+    let secRotate=scc*6;
+
+    hours.style.transform=`rotate(${hourRotate}deg)`;
+    min.style.transform=`rotate(${minRotate}deg)`;
+    sec.style.transform=`rotate(${secRotate}deg)`
+
+}
+
+setInterval(showDate,1000);
